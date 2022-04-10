c5fc7ca9a51872c4716d328dcdec80a83611bba1
Joao Kennedy Oliveira <joaokennedyoliveira@gmail.com>
2022-04-10 15:09:39 -0300

    A* Versão Final

--- a-estrela-estrategia-2/lib/NodeEstrela.py
+++ a-estrela-estrategia-2/lib/NodeEstrela.py
@@ -2,7 +2,7 @@ class NodeEstrela:
     def __init__(self, estadoAtual):
         self.estadoAtual = estadoAtual
         self.acao = None                #   Tupla (NoAtual, NoVizinho)
-        self.caminhoPercorrido = None   #   Lista contendo todas as ações até o presente nó
+        self.caminhoPercorrido = None   #   Lista Contendo todas as ações até o presente nó, ou seja, caminho da origem até o nó atual
         self.custoTotal = None          #   Resultado da função de Avaliação
     
     def __str__(self):
@@ -10,5 +10,10 @@ class NodeEstrela:
                     (self.estadoAtual, self.acao, self.custoTotal) 
     
     def printCaminhoPercorrido(self):
+        custoRealTotal = 0
         for i in self.caminhoPercorrido:
+            custoRealTotal += i['custo']
             print(i)
+
+        print("-------------------------------")
+        print("Custo total do caminho: ", custoRealTotal, "\n")
